31aee2e89efca6caf6dbcbf1b09362fb82262143
Atli Freyr Einarsson <asfalt@i-c39488ed.greenqloud.internal>
2014-10-17 18:27:27 +0000

    Added delimiter handling to Calculator

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,6 +6,11 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
+		else if(text.contains("//")){
+			String delimiter = Character.toString(text.charAt(2));
+			String newStr = text.substring(4);
+			return sum(splitDelimiter(newStr, delimiter));
+		}
 		else if(text.contains(",") || text.contains("\n")){
 			return sum(splitNumbers(text));
 		}
@@ -16,18 +21,19 @@ public class Calculator {
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
-
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",|\n");
+		return numbers.split(",|\n");
 	}
-      
-    private static int sum(String[] numbers){
- 	    int total = 0;
-        for(String number : numbers){
-		    total += toInt(number);
+	private static String[] splitDelimiter(String numbers, String del) {
+		return numbers.split(del);
+        }
+	private static int sum(String[] numbers){
+		int total = 0;
+        	for(String number : numbers){
+			total += toInt(number);
 		}
 		return total;
-    }
+	}
 
 
 
