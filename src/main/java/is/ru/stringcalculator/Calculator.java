9d00adfeadaf7bc8f92a80641e7304b94ae36ba2
Atli Freyr Einarsson <asfalt@i-c39488ed.greenqloud.internal>
2014-10-17 22:50:23 +0000

    Refactored delimiter handling

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,9 +7,7 @@ public class Calculator {
 			return 0;
 		}
 		else if(text.contains("//")){
-			String delimiter = Character.toString(text.charAt(2));
-			String newStr = text.substring(4);
-			return sum(splitDelimiter(newStr, delimiter));
+			return sum(splitDelimiter(text, getDel(text)));
 		}
 		else if(text.contains(",") || text.contains("\n")){
 			return sum(splitNumbers(text));
@@ -18,6 +16,9 @@ public class Calculator {
 			return 1;
 	}
 
+	private static String getDel(String numbers){
+		return Character.toString(numbers.charAt(2));
+	}
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
@@ -25,7 +26,7 @@ public class Calculator {
 		return numbers.split(",|\n");
 	}
 	private static String[] splitDelimiter(String numbers, String del) {
-		return numbers.split(del);
+		return (numbers.substring(4)).split(del);
         }
 	private static int sum(String[] numbers){
 		int total = 0;
